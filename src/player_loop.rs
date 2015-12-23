67895bee06254954484d50b7a75433ceb615998e
Richard Lettich <lettrich42@gmail.com>
2015-12-23 11:49:18 -0500

    Update License to BSD

--- src/player_loop.rs
+++ src/player_loop.rs
@@ -1,3 +1,36 @@
+/*
+Tunnul
+Copyright (c) 2015, Richard Lettich
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the author may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+--------------------------------------------------------------------------
+
+THIS IS NOT AN OFFICIAL MINECRAFT PRODUCT,
+NEITHER APPROVED NOR ASSOCIATED WITH MOJANG.
+*/
+
 // Array of packet handler, instead of doing a huge match
 // We just take the packet ID, packet_handler[packet.id]
 // (packet id 0 is position 0),
@@ -41,7 +74,7 @@ pub fn player_loop(mut stream:  Box<TcpStream>) {
     player.send_location();
     loop {}
 }
-    loop {
+   /* loop {
         let packet = packet::Packet::new(&mut stream);
         match packet.id {
             0..packet_handler.len() => (),
@@ -61,5 +94,5 @@ impl Player {
     fn chat_message(&packet) {
         let message = try!(inputpacket.get_string());
     }
-}x
+}*/
 
